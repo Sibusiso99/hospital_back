315a798a8614c907b7129437cc4c78e6625f2df6
sibusisoradebe <sibusiso@databalk.nu>
2023-11-01 23:34:47 +0200

    working

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(express);
 
+// connecting to the database
+
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -23,4 +25,13 @@ connection.connect((error) => {
     }else{
         console.log('Connected!!')
     }
+})
+
+//registering a doctor
+app.post("/doctor_registration", (req,res) =>{
+    const {id_no,name,surname,user_name,password} =  req.body;
+    bcrypt.hash(password,15).then((hash) =>{
+        
+
+        })
 })
